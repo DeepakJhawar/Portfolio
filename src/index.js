9af966821de2759ca37e04f76fbd5431f4a1aea2
Deepak Jhawar <deepakjhawar04@gmail.com>
2024-06-15 17:39:01 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-/**
- * Entry point of application, where App is rendered within the div with the id of "app" 
- */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import React from "react";
-import { render } from "react-dom";
-
-import App from "./App";
-
-render(<App></App>, document.getElementById("app"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
